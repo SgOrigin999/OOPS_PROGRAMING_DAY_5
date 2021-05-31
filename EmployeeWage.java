951955b83bd86df3d64577534520f0654d8babd0
SgOrigin999 <you@example.com>
2021-05-31 18:40:51 +0530

    Calculate Wage For Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,14 @@ package com.bridgelab.employeewage.day5;
 
 public class EmployeeWage {
 	public static final int FULL_TIME = 1;
+	public static final int HALF_TIME = 0;
 	public static int WAGEPERHOUR = 20;
-	public static int FULL_TIME_HRS=8;
-	public static int HALF_TIME_HRS=4;
-	public  void checkAvilablity() {
+	public static int FULL_TIME_HRS = 8;
+	public static int HALF_TIME_HRS = 4;
+	public static int NUM_OF_WORKING_DAYS = 20;
+	public static int MAX_WORKING_HRS = 100;
+
+	public void checkAvilablity() {
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		System.out.println("Emp Check Value::" + empCheck);
 		if (empCheck == FULL_TIME) {
@@ -14,8 +18,8 @@ public class EmployeeWage {
 			System.out.println("Employee is not Present");
 		}
 	}
-	
-	public  int  calculateDailyWage() {
+
+	public int calculateDailyWage() {
 		int totalWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == 1) {
@@ -26,10 +30,10 @@ public class EmployeeWage {
 		}
 		return totalWage;
 	}
-	
-	public int  partTimeWage() {
+
+	public double partTimeWage() {
 		int totalWage = 0;
-		int partTimrHrs=8;
+		int partTimrHrs = 8;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == 1) {
 			totalWage = WAGEPERHOUR * partTimrHrs;
@@ -39,14 +43,38 @@ public class EmployeeWage {
 		}
 		return totalWage;
 	}
-	
+
+	public double wageForMonth() {
+		int empHrs;
+		int empWage;
+		int totalWage = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case FULL_TIME:
+				empHrs = 8;
+				break;
+			case HALF_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = WAGEPERHOUR * empHrs;
+			totalWage = totalWage + empWage;
+		}
+		return totalWage;
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Day Five::");
-		EmployeeWage emp= new EmployeeWage();
+		EmployeeWage emp = new EmployeeWage();
 		System.out.println("Check Employee Avilablity::");
 		emp.checkAvilablity();
-		System.out.println("Calculate Daily Wage::"+emp.calculateDailyWage());
-		System.out.println("Add Part Time wage::"+emp.partTimeWage());	
-		
+		System.out.println("Calculate Daily Wage::" + emp.calculateDailyWage());
+		System.out.println("Add Part Time wage::" + emp.partTimeWage());
+		System.out.println("Calculate  Wage For Month::" + emp.wageForMonth());
+
 	}
 }
