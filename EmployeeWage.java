ad0f4d372e8ec6ed8f5180b817877bd58e37a503
SgOrigin999 <you@example.com>
2021-05-31 17:53:16 +0530

    Check Eployee Present or Not

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,21 @@
 package com.bridgelab.employeewage.day5;
 
 public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation Day Five::");
-		
+	public static final int FULL_TIME = 1;
+	public  void checkAvilablity() {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		System.out.println("Emp Check Value::" + empCheck);
+		if (empCheck == FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is not Present");
+		}
 	}
-
+	
+	public static void main(String[] args) {
+		EmployeeWage emp= new EmployeeWage();
+		System.out.println("Welcome To Employee Wage Computation Day Five::");
+		System.out.println("Check Employee Avilablity::");
+		emp.checkAvilablity();
+	}
 }
