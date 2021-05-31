85d6b53397441a76b940cf22fe500dd17c992f21
SgOrigin999 <you@example.com>
2021-05-31 17:44:35 +0530

    Calculate Employee Wage

---
+++ EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelab.employeewage.day5;
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Computation Day Five::");
+		
+	}
+
+}
