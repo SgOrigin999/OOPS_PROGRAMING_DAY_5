d5ead71d2070c373d9175dbb52763328d2e8568e
SgOrigin999 <you@example.com>
2021-05-31 18:30:40 +0530

    USe Switch Case To Calculate Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,12 @@ package com.bridgelab.employeewage.day5;
 
 public class EmployeeWage {
 	public static final int FULL_TIME = 1;
+	public static final int HALF_TIME = 0;
 	public static int WAGEPERHOUR = 20;
-	public static int FULL_TIME_HRS=8;
-	public static int HALF_TIME_HRS=4;
-	public  void checkAvilablity() {
+	public static int FULL_TIME_HRS = 8;
+	public static int HALF_TIME_HRS;
+
+	public void checkAvilablity() {
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		System.out.println("Emp Check Value::" + empCheck);
 		if (empCheck == FULL_TIME) {
@@ -14,8 +16,8 @@ public class EmployeeWage {
 			System.out.println("Employee is not Present");
 		}
 	}
-	
-	public  int  calculateDailyWage() {
+
+	public int calculateDailyWage() {
 		int totalWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == 1) {
@@ -26,10 +28,10 @@ public class EmployeeWage {
 		}
 		return totalWage;
 	}
-	
-	public int  partTimeWage() {
+
+	public int partTimeWage() {
 		int totalWage = 0;
-		int partTimrHrs=8;
+		int partTimrHrs = 8;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == 1) {
 			totalWage = WAGEPERHOUR * partTimrHrs;
@@ -39,14 +41,36 @@ public class EmployeeWage {
 		}
 		return totalWage;
 	}
-	
+
+	public double useSwitch(int random) {
+		int empCheck = random;
+		int empHrs;
+		int empWage;
+		int totalWage;
+		switch (empCheck) {
+		case HALF_TIME:
+			empHrs = 4;
+			break;
+		case FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
+		}
+		totalWage = WAGEPERHOUR * empHrs;
+		return totalWage;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Day Five::");
-		EmployeeWage emp= new EmployeeWage();
+		EmployeeWage emp = new EmployeeWage();
 		System.out.println("Check Employee Avilablity::");
 		emp.checkAvilablity();
-		System.out.println("Calculate Daily Wage::"+emp.calculateDailyWage());
-		System.out.println("Add Part Time wage::"+emp.partTimeWage());	
-		
+		System.out.println("Calculate Daily Wage::" + emp.calculateDailyWage());
+		System.out.println("Add Part Time wage::" + emp.partTimeWage());
+		int random = (int) Math.floor(Math.random() * 10) % 2;
+		double totalWage = emp.useSwitch(random);
+		System.out.println("Use Switch Case::" + totalWage);
+
 	}
 }
