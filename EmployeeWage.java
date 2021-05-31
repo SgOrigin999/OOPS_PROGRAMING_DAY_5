31927e4404879c4ff5ea679afd50b36b952a2273
SgOrigin999 <you@example.com>
2021-05-31 18:12:50 +0530

    Calculate Daily Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,8 @@ package com.bridgelab.employeewage.day5;
 
 public class EmployeeWage {
 	public static final int FULL_TIME = 1;
+	public static int WAGEPERHOUR = 20;
+	public static int FULL_TIME_HRS=8;
 	public  void checkAvilablity() {
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		System.out.println("Emp Check Value::" + empCheck);
@@ -12,10 +14,24 @@ public class EmployeeWage {
 		}
 	}
 	
+	public  int  calculateDailyWage() {
+		int totalWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1) {
+			totalWage = WAGEPERHOUR * FULL_TIME_HRS;
+			System.out.println("Employee daily Wage::" + totalWage);
+		} else {
+			System.out.println("Employee daily Wage::" + totalWage);
+		}
+		return totalWage;
+	}
+	
 	public static void main(String[] args) {
-		EmployeeWage emp= new EmployeeWage();
 		System.out.println("Welcome To Employee Wage Computation Day Five::");
+		EmployeeWage emp= new EmployeeWage();
 		System.out.println("Check Employee Avilablity::");
 		emp.checkAvilablity();
+		System.out.println("Calculate Daily Wage::"+emp.calculateDailyWage());
+		
 	}
 }
